30315578a33fd43cc22767b49808d62879c60a41
kanan <kanansnote@gmail.com>
2024-04-18 01:35:36 +0400

    Lots of work with incomplete parts of some sections, like UpBanner and Portfolio section elements; developed dropdown menus for Header link elements.

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// Implementing swiper.js to #PortfolioCards
+const cardSwiper = new Swiper('.PortfolioCards', {
+    slidesPerView: '3', // Adjust this value to control how many slides are visible at a time
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
