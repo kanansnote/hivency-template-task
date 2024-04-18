1ff16a88240ae67b0e8afc93d7fd954f83c47e3b
kanan <kanansnote@gmail.com>
2024-04-18 09:45:59 +0400

    Small work on Portfolio Section's Cards.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 // Implementing swiper.js to #PortfolioCards
-const cardSwiper = new Swiper('.PortfolioCards', {
+const cardSwiper = new Swiper('.portfolioCards', {
     slidesPerView: '3', // Adjust this value to control how many slides are visible at a time
     loop: true,
     navigation: {
