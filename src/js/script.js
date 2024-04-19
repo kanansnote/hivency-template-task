565aa111d11ab04d6bb2e07131e41ed6ca69189b
kanan <kanansnote@gmail.com>
2024-04-19 18:34:57 +0400

    Worked on Testimonial section with improving the work of Portfolio section.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,15 @@
 // Implementing swiper.js to #PortfolioCards
-const cardSwiper = new Swiper('.portfolioCards', {
+const portfolioSwiper = new Swiper('.portfolioCards', {
     slidesPerView: '3', // Adjust this value to control how many slides are visible at a time
+    loop: true,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+});
+
+const testimonialSwiper = new Swiper('.testimonialCards', {
+    slidesPerView: '1', // Adjust this value to control how many slides are visible at a time
     loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
